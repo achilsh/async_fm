1d2f941d8fec975a884ea0d68cfae33a0bef090a
achilsh <hwshtongxin@126.com>
2018-11-26 22:54:07 +0800

    增加函数指针作为线程入口函数

--- loss/src/util/mythread.cpp
+++ loss/src/util/mythread.cpp
@@ -20,6 +20,11 @@ bool MyThread::Start( std::shared_ptr<Runnable> runable )
     return true;
 }
 
+bool MyThread::Start(Callable target, void *pData)
+{
+    return  this->Start(std::make_shared<CallableHolder>(target, pData));
+}
+
 
 void MyThread::func()
 {
@@ -32,12 +37,45 @@ void MyThread::Join()
     m_ThreadData->m_Thread.join();
 }
 
+//////////////////////////////////////////////////////
+CallableHolder::CallableHolder(MyThread::Callable callable, void *pData)
+    :m_Callable( callable ), m_pData(pData)
+{
+}
+
+CallableHolder::~CallableHolder()
+{
+}
+
+void CallableHolder::Run()
+{
+    m_Callable(m_pData);
+}
+
+
 }
 
 ////////////////////////////////////////////////////////
 #if 0
+
+void test_func(void *pdata)  
+{
+    int *i_pData = (int*)pdata;
+    if (i_pData)
+    {
+        (*i_pData)  *= 10;
+        std::cout << "in func, x: " << *i_pData << std::endl;
+    }
+}
+
 int main()
 {
+    MyThread  funcThread;
+    int x = 10;
+    funcThread.Start(test_func, &x);
+    funcThread.Join();
+    std::cout << "y: " << x << std::endl;
+
     std::shared_ptr<MyRunnable> Work(new MyRunnable(100));
     MyThread testThread;
     testThread.Start( Work );
--- loss/src/util/mythread.h
+++ loss/src/util/mythread.h
@@ -17,12 +17,30 @@ class Runnable;
 class MyThread
 {
     public:
+     typedef void (*Callable)(void*);
      MyThread();
      virtual ~MyThread();
      
+     /**
+      * @brief: Start 
+      *
+      * @param runable
+      *
+      * Note that the given Runnable object must remain
+      *   valid during the entire lifetime of the thread
+      *
+      * @return 
+      */
      bool Start( std::shared_ptr<Runnable> runable );
 
+     //接收函数指针和入参作为 线程执行入口
+     bool Start(Callable target, void *pData = nullptr);
 
+     void Join();
+    private:
+     void func();
+
+    private:
      struct ThreadData
      {
          std::shared_ptr<Runnable> m_Runable;
@@ -30,11 +48,6 @@ class MyThread
          std::thread m_Thread;
      };
 
-     void Join();
-    private:
-     void func();
-
-    private:
      std::shared_ptr<ThreadData> m_ThreadData;
 };
 
@@ -47,6 +60,18 @@ class Runnable
   virtual void Run() = 0;
 };
 
+class CallableHolder: public Runnable
+{
+ public:
+  CallableHolder(MyThread::Callable callable, void *pData);
+  virtual ~CallableHolder();
+  virtual void Run(); 
+
+ private:
+  MyThread::Callable  m_Callable;
+  void* m_pData;
+
+};
 
 }
 #endif
