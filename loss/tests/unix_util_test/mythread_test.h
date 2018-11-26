4a1d380a302aa6711b8c54357a9dc40db863af27
achilsh <hwshtongxin@126.com>
2018-11-26 22:53:18 +0800

    提交测试日志

--- loss/tests/unix_util_test/mythread_test.h
+++ loss/tests/unix_util_test/mythread_test.h
@@ -32,8 +32,24 @@ void MyRunnable::Run()
         ::sleep(1);
     }
 }
+
+void test_func(void *pdata)  
+{
+    int *i_pData = (int*)pdata;
+    if (i_pData)
+    {
+        (*i_pData)  *= 10;
+        std::cout << "in func, x: " << *i_pData << std::endl;
+    }
+}
 void thread_test()
 {
+    MyThread  funcThread;
+    int x = 10;
+    funcThread.Start(test_func, &x);
+    funcThread.Join();
+    std::cout << "y: " << x << std::endl;
+
     std::shared_ptr<MyRunnable> Work(new MyRunnable(100));
     MyThread testThread;
     testThread.Start( Work );
