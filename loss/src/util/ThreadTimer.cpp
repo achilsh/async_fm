d07ee4890656d52cc9a62e92d80e0d4f8bdac5fb
achilsh <hwshtongxin@126.com>
2018-12-01 20:35:47 +0800

    修改线程实现的定时器

--- loss/src/util/ThreadTimer.cpp
+++ loss/src/util/ThreadTimer.cpp
@@ -3,23 +3,25 @@
 namespace loss
 {
 
-
 ThreadTimer::ThreadTimer( long lStartInterval, long lPeriodcInterval )
     :m_lStartInterval(lStartInterval), m_lPeriodicInterval(lPeriodcInterval),
     m_lSkippedTimes(0), p_TmrCallBack(nullptr)
 {
+    std::cout << "call ThreadTimer::ThreadTimer()" << std::endl;
 }
 
 
 ThreadTimer::~ThreadTimer()
 {
-    //todo:
+    //std::cout << "call ~ThreadTimer() -> Stop() " << std::endl;
+    //this->Stop();
 }
 
-void ThreadTimer::Start(std::shared_ptr<TimerCallbackInterface> method, ThreadPool& tp)
+void ThreadTimer::Start(std::shared_ptr<TimerCallbackInterface> method, ThreadPool* tp)
 {
-    long  lNextCallTm = 0;
-    lNextCallTm += m_lStartInterval * 1000;
+    time_point<system_clock, duration<int, std::micro>>  nextInvocation = time_point_cast<duration<int, std::micro>>(system_clock::now());
+    duration<int, std::micro> startIntervalDur(m_lStartInterval*1000);
+    nextInvocation += startIntervalDur;
 
     std::lock_guard<std::mutex> lck(m_Mutex);
     if ( p_TmrCallBack )
@@ -27,23 +29,28 @@ void ThreadTimer::Start(std::shared_ptr<TimerCallbackInterface> method, ThreadPo
         return ;
     }
 
-    m_iClockTimeNextCall = lNextCallTm;
+    m_tmPointNextCall = nextInvocation;
     p_TmrCallBack = method;
     m_evWakeUp.Reset();
 
-    tp.Start(*this);
+    std::cout << "this: " << this << std::endl;
+    std::shared_ptr<Runnable> pThreadTimerNode( this ); 
+    tp->Start(pThreadTimerNode);
 }
 
 void ThreadTimer::Stop()
 {
-    std::lock_gurad<std::mutex> lck(m_Mutex);
+    std::lock_guard<std::mutex> lck(m_Mutex);
     if (p_TmrCallBack)
     {
         m_lPeriodicInterval = 0;
         m_Mutex.unlock();
 
+        std::cout << "set wakeup notify" << std::endl;
         m_evWakeUp.Set();
+        std::cout << "wait done notify" << std::endl;
         m_evDone.Wait();
+        std::cout << "stop ThreadTimer succ" << std::endl;
     
         m_Mutex.lock();
         p_TmrCallBack = nullptr;
@@ -111,43 +118,51 @@ void ThreadTimer::SetPeriodicInterval(long ms)
 
 void ThreadTimer::Run()
 {
-    long now_tm = time(NULL);
-    long tm_interval = 0;
+    time_point<system_clock, duration<int,std::micro>> now_tm_micro = time_point_cast<duration<int, std::micro>>(system_clock::now());
+    long tm_interval_ms = 0;
     do 
     {
-        long sleep_tm(0);
-        do
-        {
-            now_tm = time(NULL);
-            sleep_tm =  (m_iClockTimeNextCall - now_tm )/1000;
-            if ( sleep_tm  < 0 )
+        long sleep_tm_ms(0);
+        do {
+
+            now_tm_micro =  time_point_cast<duration<int, std::micro>>(system_clock::now());
+            duration<int, std::micro> diff_tm = m_tmPointNextCall - now_tm_micro;
+            sleep_tm_ms = diff_tm.count()/1000;
+            if ( sleep_tm_ms < 0 )
             {
-                if ( tm_interval == 0 )
+                if ( tm_interval_ms == 0 )
                 {
-                    sleep_tm = 0;
+                    sleep_tm_ms = 0;
                     break;
                 }
-                m_iClockTimeNextCall +=  tm_interval *1000;
+
+                duration<int, std::micro> addDurTm( tm_interval_ms * 1000 );
+                m_tmPointNextCall += addDurTm;
                 ++m_lSkippedTimes;
             } 
-        } while(sleep_tm < 0);
+        } while(sleep_tm_ms < 0);
 
-        if (m_evWakeUp.TryWait(sleep_tm))
+        if (m_evWakeUp.TryWait(sleep_tm_ms))
         {
             std::lock_guard<std::mutex> lock(m_Mutex);
-            m_iClockTimeNextCall = time(NULL);
-            tm_interval = m_lPeriodicInterval;
+            m_tmPointNextCall = time_point_cast<duration<int, std::micro>>(system_clock::now());
+            tm_interval_ms = m_lPeriodicInterval;
+            usleep(1);
         }
         else
         {
             p_TmrCallBack->Invoke(*this);
             std::lock_guard<std::mutex> lock(m_Mutex);
-            tm_interval = m_lPeriodicInterval;
+            tm_interval_ms = m_lPeriodicInterval;
         }
-        m_iClockTimeNextCall += tm_interval * 1000;
+
+        duration<int, std::micro> Interval_duration(tm_interval_ms * 1000);
+        m_tmPointNextCall += Interval_duration;
         m_lSkippedTimes = 0;
 
-    } while( tm_interval > 0 );
+    } while( tm_interval_ms > 0 );
+
+    std::cout << "set evDone notify" << std::endl;
     m_evDone.Set();
 }
 
@@ -157,27 +172,21 @@ long ThreadTimer::Skipped() const
 }
 
 ///////////////////////////////////////////////////
-
 TimerCallbackInterface::TimerCallbackInterface()
 {
-    
 }
 
 TimerCallbackInterface::TimerCallbackInterface(const TimerCallbackInterface& callback) 
 {
-
 }
 
 TimerCallbackInterface::~TimerCallbackInterface() 
 {
-
 }
 
 TimerCallbackInterface& TimerCallbackInterface::operator = (const TimerCallbackInterface& tmrInterface)
 {
     return *this;
 }
-
-
 
 }
--- loss/src/util/ThreadTimer.h
+++ loss/src/util/ThreadTimer.h
@@ -2,74 +2,157 @@
 #define _THREADTIMER_H_
 
 /**
-This class implements a thread-based timer.
-A timer starts a thread that first waits for a given start interval.
-Once that interval expires, the timer callback is called repeatedly
-in the given periodic interval. If the interval is 0, the timer is only
-called once.
-The timer callback method can stop the timer by setting the 
-timer's periodic interval to 0.
+* This class implements a thread-based timer.
+* A timer starts a thread that first waits for a given start interval.
+* Once that interval expires, the timer callback is called repeatedly
+* in the given periodic interval. If the interval is 0, the timer is only
+* called once.
+* The timer callback method can stop the timer by setting the 
+* timer's periodic interval to 0.
 
-The timer callback runs in its own thread, so multithreading
-issues (proper synchronization) have to be considered when writing 
-the callback method.
+* The timer callback runs in its own thread, so multithreading
+* issues (proper synchronization) have to be considered when writing 
+* the callback method.
 
-The exact interval at which the callback is called depends on many 
-factors like operating system, CPU performance and system load and
-may differ from the specified interval.
+* The exact interval at which the callback is called depends on many 
+* factors like operating system, CPU performance and system load and
+* may differ from the specified interval.
 
-The time needed to execute the timer callback is not included
-in the interval between invocations. For example, if the interval
-is 500 milliseconds, and the callback needs 400 milliseconds to
-execute, the callback function is nevertheless called every 500
-milliseconds. If the callback takes longer to execute than the
-interval, the callback function will not be called until the next
-proper interval. The number of skipped invocations since the last
-invocation will be recorded and can be obtained by the callback
-by calling skipped().
+* The time needed to execute the timer callback is not included
+* in the interval between invocations. For example, if the interval
+* is 500 milliseconds, and the callback needs 400 milliseconds to
+* execute, the callback function is nevertheless called every 500
+* milliseconds. If the callback takes longer to execute than the
+* interval, the callback function will not be called until the next
+* proper interval. The number of skipped invocations since the last
+* invocation will be recorded and can be obtained by the callback
+* by calling skipped().
 
-The timer thread is taken from a thread pool, so
-there is a limit to the number of available concurrent timers.
+* The timer thread is taken from a thread pool, so
+* there is a limit to the number of available concurrent timers.
 ***/
+
+#include <memory>
+#include <mutex>
+#include <chrono>
+
+#include "Event.h"
+#include "ThreadPool.h"
+#include "mythread.h"
+
+using namespace std::chrono;
 
 namespace loss
 {
 
-class ThreadTimer: protected Runnable
+class TimerCallbackInterface;
+
+class ThreadTimer: public Runnable
 {
  public:
+  /**
+   * @brief: ThreadTimer 
+   *    Creates a new timer object. StartInterval and periodicInterval
+   *    are given in milliseconds. If a lPeriodcInterval of 0 is specified,
+   *    the callbacke will only be called once, after lStartInterval expires
+   *
+   * @param lStartInterval, milliseconds 
+   * @param lPeriodcInterval, milliseconds
+   */
   ThreadTimer( long lStartInterval = 0, long lPeriodcInterval = 0 );
   virtual ~ThreadTimer();
-  void Start(std::shared_ptr<TimerCallbackInterface> method, ThreadPool& tp);
+  /**
+   * @brief: Start 
+   *    Start the timer
+   *    create the TimerCallback as follows:
+   *    std::shared_ptr<TimerCallback<MyClass>> callback(new (*this, &MyClass::OnTimer));
+   *    timer.Start( callbacke, tp )
+   *
+   * @param method
+   * @param tp
+   */
+  void Start(std::shared_ptr<TimerCallbackInterface> method, ThreadPool* tp);
+
+  /**
+   * @brief: Stop 
+   *    Stops the timer. If the callback method is currently running,
+   *    it will be allowed to finish first.
+   *
+   *    WARNING: Never call this method from within the callback method,
+   *    as a deadlock would result. To stop the timer from within the callback
+   *    method, call Restart(0)
+   *
+   */
   void Stop();
+
+  /**
+   * @brief: Restart 
+   *    Restarts the periodic interval. If the callback method is already
+   *    running, nothing will happen.
+   */
   void Restart();
+
+  /**
+   * @brief: Restart 
+   *    Sets a new periodic interval and restarts the timer. An interval of
+   *    ms(0) will stop the timer.
+   *    
+   * @param ms
+   */
   void Restart(long ms);
   long GetStartInterval() const;
+
+  /**
+   * @brief: SetStartInterval 
+   *    Sets the start interval. Will only be effective before Start() is
+   *    called.
+   * @param ms
+   */
   void SetStartInterval(long ms);
   long GetPeriodicInterval() const;
+
+  /**
+   * @brief: SetPeriodicInterval 
+   *    Sets the periodic interval. If the timer is already running 
+   *    the new interval will be effective when the current interval expires
+   * @param ms
+   */
   void SetPeriodicInterval(long ms);
+
+  /**
+   * @brief: Skipped 
+   *    Returns the number of skipped invocations since the last invocation.
+   *    Skipped invocations happen if the timer callback function takes 
+   *    longer to execute than the timer interval.
+   * @return 
+   */
   long Skipped() const;
 
  protected:
   virtual void Run();
 
  private:
-  volatile long  m_lStartInterval;
-  volatile long  m_lPeriodicInterval;
+  volatile long  m_lStartInterval;    //ms
+  volatile long  m_lPeriodicInterval; //ms
 
   Event          m_evWakeUp;
   Event          m_evDone;
   long           m_lSkippedTimes;
 
   std::shared_ptr<TimerCallbackInterface>  p_TmrCallBack;
-  //TODO:
-  long                     m_iClockTimeNextCall;
+  time_point<system_clock, duration<int,std::micro>>  m_tmPointNextCall;
   mutable std::mutex       m_Mutex;
 
   ThreadTimer(const ThreadTimer&);
   ThreadTimer& operator = (const ThreadTimer&);
 };
 
+
+/**
+ * @brief:  This is the base class for all instantiations of the TimerCallback
+ * template.
+ *  
+ */
 class TimerCallbackInterface
 {
  public:
@@ -79,31 +162,36 @@ class TimerCallbackInterface
 
   TimerCallbackInterface& operator = (const TimerCallbackInterface& tmrInterface);
   virtual void Invoke( ThreadTimer& tmr ) const  = 0;
-  //TODO:
+  virtual std::shared_ptr<TimerCallbackInterface> Clone() const = 0;
 };
 
 /**
-*  This template class implements an adapter that sits between
-*  a Timer and an object's method invoked by the timer.
-*  See the Timer class for information on how
-*  to use this template class.
-*
- * */
+ * @brief: 
+ *
+ *  This template class implements an adapter that sits between
+ *  a Timer and an object's method invoked by the timer.
+ *  See the Timer class for information on how
+ *  to use this template class.
+ *
+ */
 
 template<class C>
 class TimerCallback: public TimerCallbackInterface
 {
  public:
-  typedef void (C::*Callback)(ThreadTimer& );
+  typedef void (C::*Callback)(ThreadTimer&);
 
   TimerCallback(std::shared_ptr<C> obj, Callback method): m_pObj(obj), m_Method(method) {
   }
 
-  TimerCallback(const TimerCallback& tmrcallback): m_pObj(tmrcallback.m_pObj),
-    m_Method(tmrcallback.m_Method) {
+  TimerCallback(const TimerCallback& tmrcallback): m_pObj(tmrcallback.m_pObj), m_Method(tmrcallback.m_Method) 
+  {
   }
+
   virtual ~TimerCallback()
-  {  }
+  {
+  }
+
   TimerCallback& operator = ( const TimerCallback& tmrcallback )
   { 
       if (&tmrcallback != this)
@@ -118,15 +206,20 @@ class TimerCallback: public TimerCallbackInterface
   {
       if (m_pObj)
       {
-          (m_pObj->*m_Method)(tmr);
+          (m_pObj.get()->*m_Method)(tmr);
       }
   }
 
+  virtual std::shared_ptr<TimerCallbackInterface> Clone() const 
+  {
+    return std::make_shared<TimerCallback>(*this);
+  }
  private:
   TimerCallback();
   std::shared_ptr<C>    m_pObj;
   Callback              m_Method;
 };
+
 /////////////////////////////////////////////////////////////////
 
 }
