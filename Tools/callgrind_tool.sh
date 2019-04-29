d19a0494f55037b119f2883f7c8d5011d660f933
achilsh <hwshtongxin@126.com>
2019-04-29 17:24:03 +0800

    Update callgrind_tool.sh

--- Tools/callgrind_tool.sh
+++ Tools/callgrind_tool.sh
@@ -1,7 +1,7 @@
 #! /bin/bash 
 
 BinRunFile=./test
-ret=`valgrind --tool=callgrind   ${BinRunFile}`
+ret=`valgrind --tool=callgrind  --separate-threads=yes  ${BinRunFile}`
 echo $ret
 
 `python gprof2dot.py -f callgrind -n10 -s callgrind.out.12345 > valgrind.dot`
@@ -11,6 +11,6 @@ echo $ret
 ## valgrind --tool=memcheck --log-file=   --trace-children=yes
 
 #### or use others #####
-##  用kcachegrind 打开callgrind.out.4798 
+##  用kcachegrind 打开 callgrind.out.12345 
 ##  LINUX： http://kcachegrind.sourceforge.net/html/Home.html
 ##  windows: https://sourceforge.net/projects/precompiledbin/files/?source=navbar
